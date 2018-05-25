ca33d752e5d414becb6fd9dbdb9b0d80bbe501ba
David Mann <dmann@loggerhead.com>
2018-05-25 13:53:03 -0400

    updated version and hex file

--- Arduino/snap/snap.ino
+++ Arduino/snap/snap.ino
@@ -51,7 +51,7 @@ Adafruit_SSD1306 display(OLED_RESET);
 // set this to the hardware serial port you wish to use
 #define HWSERIAL Serial1
 
-char codeVersion[12] = "2018-05-15";
+char codeVersion[12] = "2018-05-25";
 static boolean printDiags = 0;  // 1: serial print diagnostics; 0: no diagnostics
 static uint8_t myID[8];
 
