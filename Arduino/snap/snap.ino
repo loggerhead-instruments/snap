bd4629d60e626fb44663991e4c731e08c846a2e0
David Mann <dmann@loggerhead.com>
2021-06-14 17:11:21 -0400

    update version number

--- Arduino/snap/snap.ino
+++ Arduino/snap/snap.ino
@@ -13,7 +13,7 @@
 // Modified by WMXZ 15-05-2018 for SdFS anf multiple sampling frequencies
 // Optionally uses SdFS from Bill Greiman https://github.com/greiman/SdFs; but has higher current draw in sleep
 
-char codeVersion[12] = "2021-06-08";
+char codeVersion[12] = "2021-06-14";
 static boolean printDiags = 1;  // 1: serial print diagnostics; 0: no diagnostics
 
 #define USE_SDFS 0  // to be used for exFAT but works also for FAT16/32
